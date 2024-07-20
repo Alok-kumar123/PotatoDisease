b2e3d3263db951581733c265b0d7ae8c06ff1ffd
Alok-kumar123 <124078764+Alok-kumar123@users.noreply.github.com>
2024-07-21 02:05:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,10 +13,11 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "https://potato-disease-client-6idkob1dl-alok-kumars-projects-8add5b2c.vercel.app/",
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
