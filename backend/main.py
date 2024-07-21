5bd271d30b31330e769a4fe2ea6b64322a9ef8a9
Alok-kumar123 <124078764+Alok-kumar123@users.noreply.github.com>
2024-07-21 12:04:47 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,42 +4,46 @@ from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import numpy as np
 import io
-import keras
-
- 
+from tensorflow import keras
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
 app = FastAPI()
 
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "https://potato-disease-client-6idkob1dl-alok-kumars-projects-8add5b2c.vercel.app",
+    "https://potato-disease-client.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="https://potato-disease-client.vercel.app",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-#Load Model
-model=keras.models.load_model('model.h5')
+# Define custom loss
+def custom_sparse_categorical_crossentropy():
+    return SparseCategoricalCrossentropy()
 
-#Endpoint for image classification
+# Load Model
+model = keras.models.load_model(
+    'model.h5',
+    custom_objects={"SparseCategoricalCrossentropy": custom_sparse_categorical_crossentropy}
+)
 
+# Endpoint for image classification
 @app.post("/classify/")
-async def classify_image(file: UploadFile=File(...)):
+async def classify_image(file: UploadFile = File(...)):
     try:
-        contents=await file.read()
-        image=Image.open(io.BytesIO(contents))
-        img=np.array(image)
-        img=np.expand_dims(img,axis=0)
-        predictions=model.predict(img)
-        pred_class=np.argmax(predictions[0])
-        conf=round(np.max(predictions[0])*100,2)
-        class_names=["Early Blight","Late Blight","Healthy"]
-        return JSONResponse(content={"Pred_class":class_names[int(pred_class)], "Confidence":float(conf)})
+        contents = await file.read()
+        image = Image.open(io.BytesIO(contents))
+        img = np.array(image)
+        img = np.expand_dims(img, axis=0)
+        predictions = model.predict(img)
+        pred_class = np.argmax(predictions[0])
+        conf = round(np.max(predictions[0]) * 100, 2)
+        class_names = ["Early Blight", "Late Blight", "Healthy"]
+        return JSONResponse(content={"Pred_class": class_names[int(pred_class)], "Confidence": float(conf)})
     except Exception as e:
-
-        return JSONResponse(content={"error":str(e)},status_code=500)
+        return JSONResponse(content={"error": str(e)}, status_code=500)
